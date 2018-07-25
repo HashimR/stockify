b83a190a038a93c2c3204b250914bcfaf8a0f148
Hashim <hashim.rehman@hotmail.co.uk>
2018-07-25 16:42:04 +0100

    Write open price during trading hours

--- server/stock_data_generator.py
+++ server/stock_data_generator.py
@@ -2,6 +2,8 @@
 
 import requests
 import csv
+import datetime
+from pytz import timezone
 
 baseURL = "https://api.iextrading.com/1.0/stock/"
 
@@ -13,7 +15,20 @@ def generate_data(filename, stock_code, timeframe):
         writer = csv.DictWriter(csvfile, fieldnames)
         writer.writeheader()
         writer.writerows(data)
+    if(is_trading_hours):
+        write_today_open(stock_code, filename)
 
+def write_today_open(stock_code, filename):
+    response = requests.get("https://api.iextrading.com/1.0/stock/" + stock_code + "/ohlc")
+    data = response.json()
+    price = data['open']['price']
+    date = datetime.datetime.now()
+    date = date.strftime('%Y-%m-%d')
+    with open(filename, 'a') as csvfile:
+        fieldnames = ['date', 'open', 'high', 'low', 'close', 'volume', 'unadjustedVolume', 'change', 'changePercent', 'vwap', 'label', 'changeOverTime']
+        open_writer = csv.DictWriter(csvfile, fieldnames)
+        open_writer.writerow({'date': date, 'open': price})
+    
 def generate_training_data(stock_code):
     filename = "training_data_" + stock_code + ".csv"
     generate_data(filename, stock_code, "5y")
@@ -21,3 +36,7 @@ def generate_training_data(stock_code):
 def generate_test_data(stock_code):
     filename = "test_data_" + stock_code + ".csv"
     generate_data(filename, stock_code, "3m")
+
+def is_trading_hours():
+    tz = timezone('EST')
+    return datetime.time(9, 30) < datetime.datetime.time(datetime.datetime.now(tz)) < datetime.time(16, 30)
